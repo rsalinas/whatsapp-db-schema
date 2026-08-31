048374fa3441f9466e55e080ae8e725041363600
Raul Salinas <rausamon@upv.es>
2026-08-31 02:27:29 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -3564,6 +3564,10 @@ CREATE TRIGGER message_bd_for_message_media_ai_provenance_trigger BEFORE DELETE 
 CREATE TRIGGER message_bd_for_message_music_trigger BEFORE DELETE ON message BEGIN DELETE FROM message_music WHERE message_row_id=old._id; END;
 CREATE TRIGGER message_music_bd_for_message_media_interactive_annotation_trigger BEFORE DELETE ON message_music BEGIN DELETE FROM message_media_interactive_annotation WHERE message_row_id=old.message_row_id; END;
 CREATE TABLE newsletter_admin_profile(_id INTEGER PRIMARY KEY AUTOINCREMENT,chat_row_id INTEGER NOT NULL,admin_profile_id TEXT NOT NULL,name TEXT,picture_id TEXT,picture_direct_path TEXT,timestamp INTEGER NOT NULL);
+CREATE TRIGGER chat_bd_for_newsletter_admin_profile_trigger BEFORE DELETE ON chat BEGIN DELETE FROM newsletter_admin_profile WHERE chat_row_id=old._id; END;
+CREATE TRIGGER message_bd_for_experience_id_trigger BEFORE DELETE ON message BEGIN DELETE FROM experience_id WHERE message_row_id=old._id; END;
+CREATE UNIQUE INDEX newsletter_admin_profile_unique_index
+            ON newsletter_admin_profile (chat_row_id, admin_profile_id);
 CREATE VIEW available_message_view AS
             SELECT
               
@@ -3976,7 +3980,3 @@ CREATE VIEW chat_view AS
                 chat.jid_row_id AS original_jid_row_id
             FROM chat AS chat
 /* chat_view(_id,hidden,subject,created_timestamp,last_message_row_id,display_message_row_id,last_read_message_row_id,last_read_receipt_sent_message_row_id,last_important_message_row_id,archived,sort_timestamp,mod_tag,gen,spam_detection,unseen_earliest_message_received_time,unseen_message_count,unseen_missed_calls_count,unseen_row_count,unseen_message_reaction_count,unseen_comment_message_count,last_message_reaction_row_id,last_seen_message_reaction_row_id,plaintext_disabled,vcard_ui_dismissed,change_number_notified_message_row_id,show_group_description,ephemeral_expiration,ephemeral_setting_timestamp,ephemeral_displayed_exemptions,ephemeral_disappearing_messages_initiator,unseen_important_message_count,group_type,growth_lock_level,growth_lock_expiration_ts,last_read_message_sort_id,display_message_sort_id,last_message_sort_id,last_read_receipt_sent_message_sort_id,has_new_community_admin_dialog_been_acknowledged,history_sync_progress,chat_lock,chat_origin,participation_status,chat_encryption_state,group_member_count,limited_sharing,limited_sharing_setting_timestamp,is_contact,ephemeral_after_read_duration,business_chat_state,chat_props,jid_row_id,original_jid_row_id) */;
-CREATE TRIGGER chat_bd_for_newsletter_admin_profile_trigger BEFORE DELETE ON chat BEGIN DELETE FROM newsletter_admin_profile WHERE chat_row_id=old._id; END;
-CREATE TRIGGER message_bd_for_experience_id_trigger BEFORE DELETE ON message BEGIN DELETE FROM experience_id WHERE message_row_id=old._id; END;
-CREATE UNIQUE INDEX newsletter_admin_profile_unique_index
-            ON newsletter_admin_profile (chat_row_id, admin_profile_id);
